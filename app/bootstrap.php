ca76676be98c5db326042ddc2f1e387dc1aea62e
Michael Sypolt <msypolt@gmail.com>
2015-04-24 07:59:15 -0400

    Minor tweaks

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,7 +10,7 @@
  * @category   Bootstrap
  * @package    Serverphu
  * @author     Michael Sypolt <msypolt@transitguru.limited>
- * @copyright  Copyright (c) 2014
+ * @copyright  Copyright (c) 2015
  * @license    http://www.gnu.org/licenses/gpl-3.0.txt
  * @version    @package_version@
  */
--- app/model/test.php
+++
@@ -1,3 +0,0 @@
-Model...<br />
-<?php 
-// Future settings file
